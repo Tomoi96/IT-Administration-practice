5c83cb1666b3fbd662da6a02e22941fb5c220f39
Tomoi96 <tomislav.lazaret@gmail.com>
2023-07-12 21:54:22 +0200

    Servers variables and adding new server

---
+++ it-admin.ps1
@@ -0,0 +1,6 @@
+$dbServers = @("DatabaseServer1","DatabaseServer2")
+$webServers = @("WebServer1","WebServer2","WebServer3")
+$storageServers = @("StorageServer1")
+
+#Adding new server below
+$server = “StorageServer4”
